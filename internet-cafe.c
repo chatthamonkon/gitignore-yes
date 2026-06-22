141e8f3d12099480a2c809fb07619c10d04acf05
chatthamonkon <chattiger53@gmail.com>
2026-06-22 10:37:01 +0700

    อัปเดตแก้ไขโค้ด internet-cafe

--- internet-cafe.c
+++ internet-cafe.c
@@ -1,8 +1,11 @@
-#include  <stdio.h>
-int main() {    
-    int hours = 50;
-    int rate = 15;
-    int total_cost = hours * rate;
-    printf("Total cost: %d bath\n", total_cost);
-    return 0;   
+#include <stdio.h>
+int main() {
+    //internet_cafe.cLevel 1 : แบบพื้นฐาน (Basic)1. ร้านอินเตอร์เน็ตคาเฟเร้านหนึ่งคิดค่าใช้บริการ 15 บาทต่อชั่วโมง จงคำานวณค่าใช้จ่ายของลูกค้าที่ใช้บริการเป็นเวลา50 ชั่วโมง ใช้้กลอกข้อมูลลงไป
+    int hours,cost;
+    printf("Enter hours: ");
+    scanf("%d",&hours); //50
+    printf("Enter cost: ");
+    scanf("%d",&cost); //15
+    int total = hours * cost;
+    printf("Total cost = %d",total);
 }
